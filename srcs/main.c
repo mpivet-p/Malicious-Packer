4c02094ad87c0f57f33394e55c533fe36057de7d
mpivet-p <mpivet-p@student.42.fr>
2022-10-02 13:31:28 +0200

    Refacto done. Ready to try inserting code

--- srcs/main.c
+++ srcs/main.c
@@ -6,147 +6,132 @@
 #include <sys/mman.h>
 #include <errno.h>
 
-//static int		get_file(const char *filepath, void **file_content, size_t *file_size)
-//{
-//	int			fd;
-//
-//	if ((fd = open(filepath, O_RDONLY)) < 0)
-//	{
-//		fprintf(stderr, "woody: Error opening the file.\n");
-//		return (1);
-//	}
-//	*file_size = lseek(fd, 0, SEEK_END);
-//	lseek(fd, 0, SEEK_SET);
-//	*file_content = mmap(NULL, *file_size, PROT_READ, MAP_PRIVATE, fd, 0);
-//	close(fd);
-//	return (*file_content != MAP_FAILED);
-//}
-
-void	handle_read_return(int ret)
+// Returns the virtual address of the first segment of type == PT_LOAD
+uint64_t	get_image_base(void *file)
 {
-	if (ret <= 0)
-	{
-		perror("woody: ");
-		exit(1);
-	}
-}
+	Elf64_Ehdr	*ehdr;
+	Elf64_Phdr	*phdr;
 
-uint64_t	get_image_base(int fd)
-{
-	Elf64_Ehdr	ehdr;
-	Elf64_Phdr	phdr;
-
-	lseek(fd, 0, SEEK_SET);
-	handle_read_return(read(fd, &ehdr, sizeof(ehdr)));
+	ehdr = (Elf64_Ehdr*)file;
 	
-	lseek(fd, ehdr.e_phoff, SEEK_SET);
-	for (int i = 0; i < ehdr.e_phnum; i++)
-	{
-		if (phdr.p_type == PT_LOAD)
-		{
-			printf("addr: 0x%lx\n", phdr.p_vaddr);
-			return (phdr.p_vaddr);
-		}
-		handle_read_return(read(fd, &phdr, sizeof(phdr)));
-	}
-	return (0);
-}
-
-int		get_shstrtab_content(int fd, Elf64_Ehdr *ehdr, char **ptr)
-{
-	Elf64_Shdr	shdr;
-	int			ret;
-
-	lseek(fd, ehdr->e_shoff + (sizeof(shdr) * ehdr->e_shstrndx), SEEK_SET);
-	handle_read_return(read(fd, &shdr, sizeof(shdr)));
-	lseek(fd, shdr.sh_offset, SEEK_SET);
-	if (!(*ptr = (char*)malloc((shdr.sh_size + 1) * sizeof(char))))
-		exit(2);
-	if ((ret = read(fd, *ptr, shdr.sh_size)) < (ssize_t)(shdr.sh_size))
-	{
-		free(*ptr);
-		return (1);
-	}
-	(*ptr)[ret] = 0;
-	return (0);
-}
-
-int		is_pt_load(int fd, Elf64_Ehdr *ehdr, uint64_t offset, uint64_t size)
-{
-	Elf64_Phdr	phdr;
-
-	lseek(fd, ehdr->e_phoff, SEEK_SET);
 	for (int i = 0; i < ehdr->e_phnum; i++)
 	{
-		handle_read_return(read(fd, &phdr, sizeof(phdr)));
-		if (offset >= phdr.p_offset
-			&& (offset + size) < (phdr.p_offset + phdr.p_filesz))
+		phdr = (Elf64_Phdr*)(file + ehdr->e_phoff + (sizeof(Elf64_Phdr) * i));
+		if (phdr->p_type == PT_LOAD)
 		{
-			return (phdr.p_type == PT_LOAD);
+			printf("addr: 0x%lx\n", phdr->p_vaddr);
+			return (phdr->p_vaddr);
 		}
 	}
 	return (0);
 }
 
-int		enough_space_for_payload(int fd, Elf64_Shdr *shdr)
+int		get_shstrtab_content(void *file, Elf64_Ehdr *ehdr, char **ptr)
+{
+	Elf64_Shdr	*shdr;
+
+	shdr = (Elf64_Shdr*)(file + ehdr->e_shoff + (sizeof(Elf64_Shdr) * ehdr->e_shstrndx));
+	*ptr = (char*)(file + shdr->sh_offset);
+	return (0);
+}
+
+int		is_pt_load(void *file, Elf64_Ehdr *ehdr, uint64_t offset, uint64_t size)
+{
+	Elf64_Phdr	*phdr;
+
+
+	for (int i = 0; i < ehdr->e_phnum; i++)
+	{
+		phdr = (Elf64_Phdr*)(file + ehdr->e_phoff + i * sizeof(Elf64_Phdr));
+		if (offset >= phdr->p_offset
+			&& (offset + size) < (phdr->p_offset + phdr->p_filesz))
+		{
+			return (phdr->p_type == PT_LOAD);
+		}
+	}
+	return (0);
+}
+
+int		enough_space_for_payload(void *file, Elf64_Shdr *shdr)
 {
 	char *content;
 
-	if (!(content = (char*)malloc(sizeof(char) * shdr->sh_size)))
-		exit (1);
-	lseek(fd, shdr->sh_offset, SEEK_SET);
-	handle_read_return(read(fd, content, shdr->sh_size));
-	for (int i = shdr->sh_size; i > 0; --i)
+	content = (char*)(file + shdr->sh_offset);
+	for (int i = shdr->sh_size; i > 0; i--)
 	{
-		if (content[i] != 0)
+		if (content[i - 1] != 0)
 			return (shdr->sh_size - i >= 80);
 	}
-	free(content);
 	return (0);
 }
 
-void	iterate_over_section_headers(int fd)
+void	iterate_over_section_headers(void *file)
 {
-	Elf64_Ehdr	ehdr;
-	Elf64_Shdr	shdr;
+	Elf64_Ehdr	*ehdr;
+	Elf64_Shdr	*shdr;
 	char		*shstrtab_content;
 
-	lseek(fd, 0, SEEK_SET);
-	handle_read_return(read(fd, &ehdr, sizeof(ehdr)));
-	get_shstrtab_content(fd, &ehdr, &shstrtab_content);
+	ehdr = (Elf64_Ehdr*)file;
+	get_shstrtab_content(file, ehdr, &shstrtab_content);
 
-	lseek(fd, ehdr.e_shoff, SEEK_SET);
-	for (int i = 0; i < ehdr.e_shnum; i++)
+	for (int i = 0; i < ehdr->e_shnum; i++)
 	{
-		lseek(fd, ehdr.e_shoff + (i * sizeof(shdr)), SEEK_SET);
-		handle_read_return(read(fd, &shdr, sizeof(shdr)));
-		printf("addr: %lx, offset: %lx, size: %ld, %s", shdr.sh_addr\
-		, shdr.sh_offset, shdr.sh_size, shstrtab_content + shdr.sh_name);
-		if (shdr.sh_flags & SHF_EXECINSTR)
+
+		shdr = (Elf64_Shdr*)(file + ehdr->e_shoff + (i * sizeof(Elf64_Shdr)));
+		printf("addr: %lx, offset: %lx, size: %ld, %s", shdr->sh_addr\
+		, shdr->sh_offset, shdr->sh_size, shstrtab_content + shdr->sh_name);
+		if (shdr->sh_flags & SHF_EXECINSTR)
 			printf(" EXEC");
-		if (shdr.sh_size >= 80 && is_pt_load(fd, &ehdr, shdr.sh_offset, shdr.sh_size) == 1)
-			printf(" %d", enough_space_for_payload(fd, &shdr));
+		if (shdr->sh_size >= 80 && is_pt_load(file, ehdr, shdr->sh_offset, shdr->sh_size) == 1)
+			printf(" %d", enough_space_for_payload(file, shdr));
 		printf("\n");
 	}
-	free(shstrtab_content);
 }
 
-int	main(int argc, char **argv)
+static int		get_file(const char *filepath, void **file, size_t *fsize)
 {
-	int	fd;
+	int			fd;
 
-	if (argc > 1)
+	if ((fd = open(filepath, O_RDONLY)) < 0)
 	{
-		if ((fd = open(argv[1], O_RDONLY)) >= 0)
-		{
-			get_image_base(fd);
-			iterate_over_section_headers(fd);
-		}
-		else
-		{
-			perror("woody: ");
-			return (1);
-		}
+		perror("woody: ");
+		return (1);
 	}
+	*fsize = lseek(fd, 0, SEEK_END);
+
+	lseek(fd, 0, SEEK_SET);
+	*file = mmap(NULL, *fsize, PROT_READ, MAP_PRIVATE, fd, 0);
+	close(fd);
+	if (*file == MAP_FAILED)
+	{
+		perror("woody: ");
+		return (1);
+	}
+
 	return (0);
+}
+
+int				woody(char *file_name, void *file, size_t fsize)
+{
+	(void)file_name;
+	(void)fsize;
+	get_image_base(file);
+	iterate_over_section_headers(file);
+	munmap(file, fsize);
+	return (0);
+}
+
+int				main(int argc, char **argv)
+{
+	void	*file_content = NULL;
+	size_t	file_size = 0;
+	int		ret = 0;
+
+	if (argc == 2)
+	{
+		if (get_file(argv[1], &file_content, &file_size) != 0
+			|| woody(argv[1], file_content, file_size) != 0)
+			ret = 1;
+	}
+	return (ret);
 }
